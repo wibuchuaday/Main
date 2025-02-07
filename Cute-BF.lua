32f0bce989c555c5dee30692fd506eaf7c1efe7f
wibuchuaday <hckvdae999@gmail.com>
2025-02-07 17:52:18 +0700

    Update Cute-BF.lua

--- Cute-BF.lua
+++ Cute-BF.lua
@@ -96,7 +96,7 @@ local v7 = CreateObject("ImageLabel", {
     Name = "UserImage",
     Parent = v6,
     BackgroundTransparency = 1,
-    Image = "rbxassetid://13717478897",
+    Image = "rbxassetid://7445543667",
     Position = UDim2.new(0, 15, 0, 10),
     Size = UDim2.new(0, 50, 0, 50)
 });
